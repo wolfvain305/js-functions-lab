d6e0f19bca3209993c4a9ebd10513aa8bda29b16
wolfvain305 <dwolf0305@gmail.com>
2024-05-12 11:09:56 -0400

    exercise

--- script.js
+++ script.js
@@ -299,3 +299,17 @@ function createUsername(firstName,lastName){
 
 
 console.log('Exercise 13 Result:', createUsername("Samantha", "Green"));
+
+/*
+Exercise 14: numArgs()
+
+Challenge yourself with numArgs. This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+function numArgs() {
+    return arguments.length;
+}
+
+
+console.log('Exercise 14 Result:', numArgs(1, 2, 3, 4));
